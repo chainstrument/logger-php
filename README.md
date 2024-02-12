384f591d374321359855cad2c2136c1f79a9db08
Chahir <minitutoriel@gmail.com>
2024-02-12 20:32:57 +0100

    add more infos

---
+++ README.md
@@ -0,0 +1,7 @@
+# A simple implementation of logger 
+
+> composer require monolog/monolog 
+
+### Documentation
+
+> https://github.com/Seldaek/monolog/blob/main/doc/01-usage.md
--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
 <?php 
 
+require_once '../vendor/autoload.php';
+ 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
 
-echo 'testing';
+// Create the logger
+$logger = new Logger('my_logger');
+// Now add some handlers
+$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/my_app.log' ));
+ 
+
+// You can now use your logger
+$logger->info('My logger is now ready');
